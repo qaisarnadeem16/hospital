da1d5ab228d0acca5148645fefa9869bbb2581eb
Qaisar Nadeem <101329010+qaisarnadeem16@users.noreply.github.com>
2023-07-25 16:02:11 +0500

    fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,54 @@
 import React from "react";
 import "./Footer.css";
 import Wave from "../../img/wave.png";
-// import Insta from "@iconscout/react-unicons/icons/uil-instagram";
-// import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
-// import Gitub from "@iconscout/react-unicons/icons/uil-github";
+import { Link } from "react-scroll";
+import { Link as RouterLink } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="footer">
       <img src={Wave} alt="" style={{ width: "100%" }} />
+
       <div className="f-content">
-        {/* <span>Zainkeepscode@gmail.com</span> */}
-        <div className="f-icons">
-          {/* <Insta color="white" size={"3rem"} />
-          <Facebook color="white" size={"3rem"} />
-          <Gitub color="white" size={"3rem"} /> */}
+      <div className="i-name">
+          {/* yahan change hy darkmode ka */}
+          <span >Drbuttpolyclinic</span>
+          <span>Hospital</span>
+      </div>
+
+       <div>
+          <ul style={{ listStyleType: "none" }}  className="list1">
+            <li>
+              <RouterLink to="/">
+                Home
+              </RouterLink>
+            </li>
+            <li>
+              <Link to="services" spy={true} smooth={true}>
+                Serivces
+              </Link>
+            </li>
+         
+            <li>
+              <Link to="testimonial" spy={true} smooth={true}>
+                Testimonial
+              </Link>
+            </li>
+
+            <li>
+              <Link to="opd" spy={true} smooth={true}>
+                OPD
+              </Link>
+            </li>
+
+            <li>
+              <Link to="laboratory" spy={true} smooth={true}>
+                Laboratiry
+              </Link>
+            </li>
+
+
+          </ul>
         </div>
       </div>
     </div>
